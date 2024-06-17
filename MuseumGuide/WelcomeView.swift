128df9ec69d28319374c000616b445ab4d093b65
Tolga Haliloglu <tolga.haliloglu@turkcell.com.tr>
2024-06-17 05:21:29 +0200

    adds code improvement for WelcomeView.

--- MuseumGuide/WelcomeView.swift
+++ MuseumGuide/WelcomeView.swift
@@ -11,43 +11,45 @@ struct WelcomeView: View {
     @Environment(AppModel.self) private var appModel
         
     var body: some View {
-                
         ZStack {
             NavigationStack {
                 VStack {
                     Spacer()
-
-                    // Header
-                    Text("Welcome to the Museum Guide")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding()
-
-                    // Description
-                    Text("Explore the rich history and diverse collections of our museum. Discover artifacts, learn about historical events, and enjoy interactive exhibits.")
-                        .font(.body)
-                        .multilineTextAlignment(.center)
-                        .padding()
-
+                    header
+                    description
                     Spacer()
-
-                    // Button
-                    HStack {
-                        Spacer()
-                        CollectionsToggle()
-                        ImmersiveToggle()
-                    }
-                    
+                    toggleButtons
                 }
                 .padding()
             }
             .opacity(appModel.isShowingImmersive ? 0 : 1)
-        }
-        .animation(.default, value: appModel.isShowingImmersive)
-        .overlay(alignment: .bottom) {
+            
             ImmersiveToggle()
                 .opacity(appModel.isShowingImmersive ? 1 : 0)
-                .padding()
+                .padding(.bottom)
+        }
+        .animation(.default, value: appModel.isShowingImmersive)
+    }
+    
+    private var header: some View {
+        Text("Welcome to the Museum Guide")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+    }
+    
+    private var description: some View {
+        Text("Explore the rich history and diverse collections of our museum. Discover artifacts, learn about historical events, and enjoy interactive exhibits.")
+            .font(.body)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+    
+    private var toggleButtons: some View {
+        HStack {
+            Spacer()
+            CollectionsToggle()
+            ImmersiveToggle()
         }
     }
 }
