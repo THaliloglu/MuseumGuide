285305b1306f30a0adefb6e2e1501764d7c6f924
Tolga Haliloglu <tolga.haliloglu@turkcell.com.tr>
2024-06-17 05:35:19 +0200

    add some comment on CollectionItem.

--- MuseumGuide/CollectionItem.swift
+++ MuseumGuide/CollectionItem.swift
@@ -8,9 +8,17 @@
 import Foundation
 import SwiftUI
 
+/// A model representing an item in the museum's collection.
 struct CollectionItem: Identifiable, Hashable, Codable, Equatable {
+    /// Unique identifier for the collection item.
     var id = UUID()
+    
+    /// Title of the collection item.
     let title: String
+    
+    /// Description of the collection item.
     let description: String
+    
+    /// Model name associated with the collection item.
     let modelName: String
 }
