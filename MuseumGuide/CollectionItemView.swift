34516026200ac62681162dcf0eaecb11c4506738
Tolga Haliloglu <tolga.haliloglu@turkcell.com.tr>
2024-06-17 05:47:24 +0200

    adds code improvement for CollectionItemView.

--- MuseumGuide/CollectionItemView.swift
+++ MuseumGuide/CollectionItemView.swift
@@ -6,23 +6,33 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
+/// A view to display detailed information about a specific collection item.
 struct CollectionItemView: View {
     let item: CollectionItem
     
     var body: some View {
         VStack {
+            // Display the title of the collection item
             Text(item.title)
                 .font(.headline)
                 .multilineTextAlignment(.center)
                 .padding()
+            
+            // Display the description of the collection item
+            Text(item.description)
+                .font(.body)
+                .multilineTextAlignment(.leading)
+                .padding([.leading, .trailing, .bottom])
         }
     }
 }
 
 #Preview {
-    let item = CollectionItem(title: "Aphrodite of Milos", description: "Detailed information about Aphrodite of Milos.", modelName: "aphrodite")
+    let item = CollectionItem(
+        title: "Aphrodite of Milos",
+        description: "Detailed information about Aphrodite of Milos.",
+        modelName: "aphrodite"
+    )
     return CollectionItemView(item: item)
 }
